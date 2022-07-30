e3724f4449dc964cb5dec09a7a95db41a5a8e663
“sd1875819” <sd_sunjin@163.com>
2022-07-30 15:12:05 +0800

    添加部分导航栏的调整及总结内容

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,23 +1,35 @@
 <template>
   <div>
+    <!--
+      1、<el-menu>是菜单标签，里面可以包括：<el-submenu>和<el-menu-item>。
+      2、<el-submenu>是子菜单标签，里面可以包括：<el-menu-item>。
+      3、<el-menu-item>是最低级的菜单项，可以被前两者包含。
+    -->
     <el-menu
         style="width: 200px; min-height: calc(100vh - 50px)"
         default-active="2"
         class="el-menu-vertical-demo"
     >
-<!--   一级菜单使用el-menu-item，二级菜单则在el-menu-item内使用el-sub-menu -->
-        <el-menu-item index="1-4">
-          <template #title>item four</template>
-          <el-sub-menu index="1-4-1">item one</el-sub-menu>
-        </el-menu-item>
-        <el-menu-item index="2">
+        <el-sub-menu index="1">  <!--当有子导航栏时，父类导航栏用el-sub-menu，子导航栏用el-menu-item-->
+          <template #title> <!-- template 标签是用来控制submenu对应的一级导航栏内容的显示-->
+            <i class="el-icon-location"></i>  <!--<i>里放置导航栏的图标-->
+            <span>导航一</span> <!--<span>是一级导航栏里导航栏标题的展示-->
+          </template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="2">
           <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
+          <template #title>导航二</template>
+        </el-sub-menu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
     </el-menu>
   </div>
 
