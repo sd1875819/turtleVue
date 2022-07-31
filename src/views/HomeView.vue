d2d82133580e5f26adf0acd82f7b158bc7271fdf
“sd1875819” <sd_sunjin@163.com>
2022-08-01 07:27:21 +0800

    新增编辑功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
     <div style="margin: 10px 0">
       <!--其中v-model里面的参数都要放到return里面定义才可以,否则页面会报错,放到进去之后也方便我们更好的去取值，
       通过this.xxx就能获取到输入的值，或者给该参数赋值，return中的参数通过this.xxx调用-->
-      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%" clearable/>
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%" clearable/> <!--clearable为输入框添加删除按钮，当有内容输入时显示-->
       <el-button type="primary" style="margin-left: 5px" @click="load">查询</el-button><!-- type="primary"设置组件颜色为蓝色，style="margin-left: 5px"设置距左组件的距离-->
     </div>
 <!--数据表格展示区域-->
@@ -24,7 +24,7 @@
       <el-table-column prop="address" label="地址" width="400"/>
       <el-table-column fixed="right" label="Operations" width="160">
         <template #default>
-          <el-button link type="primary" @click="handleEdit">编辑</el-button>
+          <el-button link type="primary" @click="handleEdit(scope.row)">编辑</el-button>  <!--在编辑方法里传入参数scope.row-->
           <el-popconfirm title="你确定删除吗?">
             <template #reference>
               <el-button type="text">删除</el-button> <!-- type="text"将该按钮以文本的形式展示，danger按钮的形式展示-->
@@ -131,12 +131,35 @@ export default {
     save() {
       //向后端传数据进行保存用post方法
       //form对象是在新增用户弹窗里与各个属性进行了绑定，将新增用户信息弹窗中选择的用户信息参数值包装成form对象直接传给后台;
-      request.post("/user/submit", this.form).then(res => {
-        console.log(res)
-      })
+      if (this.form.id) {  /*新增跟编辑操作都会点击确认，调用save方法。如果form对象里有id，表示此时在执行编辑更新操作。*/
+        request.put("/user/update", this.form).then(res => {
+          console.log(res)
+          if (res.code == '0') {
+            this.$message({  /*执行编辑更新成功后给一个弹窗提示*/
+              type: "success",
+              message: "更新成功"
+            })
+          }
+          this.load()  //更新成功后自动刷新表格数据
+          this.dialogVisible = false
+        })
+      } else { /*form对象里没有id表示是新增操作*/
+        request.post("/user/submit", this.form).then(res => {
+          console.log(res)
+          if (res.code == '0') {
+            this.$message({  /*执行新增成功后给一个弹窗提示*/
+              type: "success",
+              message: "新增成功"
+            })
+          }
+        })
+      }
     },
-    handleEdit() {
-
+    handleEdit(row) {  /*编辑方法需要弹窗，同时将行数据赋给弹窗的表单*/
+      /*因为表单里的数据是跟form对象绑定的，所以直接给form对象赋值即可。在输入完点击取消，为了避免数据发生变化的拷贝问题，
+      可以通过JSON.parse(JSON.stringify())对对象进行深拷贝，这样该处的from就是一个独立对象，跟之前表格里是隔离开了，避免浅拷贝问题*/
+      this.form = JSON.parse(JSON.stringify(row))
+      this.dialogVisible = true /*打开弹窗*/
     }
   }
 }
