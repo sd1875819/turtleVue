78e3ed6ac3e063670ee9f27a344c914134376131
“sd1875819” <sd_sunjin@163.com>
2022-08-03 07:21:02 +0800

    添加路由，创建登陆界面

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@ const routes = [
   {
     path: '/',  /*表示如果访问http://localhost:8081/就会跳转到layout.vue界面*/
     name: 'Layout',
-    component: Layout,   /*指定访问path配置的地址时对应打开的页面是哪个，如果path配置为'/home'，则后台启动后需要访问localhost:8081/home地址才能访问到该页面*/
+    component: Layout,   /*指定访问path配置的地址时对应打开的页面是哪个，如果访问/则打开的是Layout布局文件，只有头部跟侧边栏的页面*/
+    redirect: "/home",  /*路由重定向，当访问/时，会自动重定向到/home地址上，这样就实现了项目启动后自动打开/home的页面，避免了上一步的只访问Layout布局文件*/
     /*children里放嵌套路由*/
-    children: [
-
+    children: [  /*嵌套路由就是访问上一层里的path+children里的path就可以访问children里component指定的页面嵌套到上一层的component页面里的总页面了，即包含了头部、侧边栏即主体的页面*/
         {
           path: 'home',   /*这样就可以通过/home访问HomeView页面了*/
           name: 'Home',
@@ -16,6 +16,11 @@ const routes = [
         }
     ]
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import("@/views/Login")
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <!--设置整个登陆页面的宽度是100%， 高度是100pv,页面背景色,overflow: hidden是避免页面顶部留白-->
+  <div style="width: 100%; height: 100vh; background-color: dodgerblue; overflow: hidden">
+    <div style="width: 400px; margin: 150px auto">  <!--设置登陆框的位置：宽度width是400px，居中设置上下是150px，左右是auto自适应-->
+        <div style="color: #cccccc; font-size: 30px; text-align: center">欢迎登陆</div>   <!--text-align: center设置字体居中-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
