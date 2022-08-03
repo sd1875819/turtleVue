1c443aeb71008f5e7f83ef320be95924619bb2ec
sd_vivo2020 <sd_vivo2020@bbktel.com>
2022-08-03 17:55:20 +0800

    解决路由重定向及嵌套路由不生效问题

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,24 @@ import Layout from '../layout/Layout.vue'
 
 const routes = [
   {
-    path: '/',  /*表示如果访问http://localhost:8081/就会跳转到layout.vue界面*/
-    name: 'Layout',
-    component: Layout,   /*指定访问path配置的地址时对应打开的页面是哪个，如果访问/则打开的是Layout布局文件，只有头部跟侧边栏的页面*/
-    redirect: "/home",  /*路由重定向，当访问/时，会自动重定向到/home地址上，这样就实现了项目启动后自动打开/home的页面，避免了上一步的只访问Layout布局文件*/
-    /*children里放嵌套路由*/
+    path: '/',  /*'/'符号表示根目录，path里带该符号的都是直接拼接到http://localhost:8081后面使用的*/
+    redirect: "/home",  /*redirect是路由重定向，表示当访问带'/'根目录前台地址时，会自动重定向到/home地址上，这样就实现了项目启动后自动打开路由配置为/home里component指定的页面*/
+  },
+  {
+    path: '/home',
+    name: 'HomeView',
+    component: Layout,
+    /*children里放嵌套路由，即将chileren里component指定的HomeView页面嵌套到上一层路由/home里的component指定的Layout框架页面里，
+    即Layout是布局好了头部、侧边栏并且给主页面预留了位置(见Layout.vue文件备注)，该处嵌套的意识就是把主页面放到Layout框架页里的预留位置处进行统一展示*/
     children: [  /*嵌套路由就是访问上一层里的path+children里的path就可以访问children里component指定的页面嵌套到上一层的component页面里的总页面了，即包含了头部、侧边栏即主体的页面*/
-        {
-          path: 'home',   /*这样就可以通过/home访问HomeView页面了*/
-          name: 'Home',
-          component: () => import("@/views/HomeView"),  /*，该处是导入页面的另外一种方式，将HomeView界面进行二次路由导入*/
-        }
+    {
+      /*注意，嵌套路由里不需要再加'/'，该处加上'/'后就变成根目录了，就跟上一层的/home平级，
+      该处我们是需要将HomeView页面嵌套到上一层路由/home指定的Layout框架页中：通过http://localhost:8081/home/homeView访问页面
+      但是该处有个疑问，该处写成path: '/homeView'后，通过访问http://localhost:8081/homeView也是可以将homeView嵌套到Layout框架页中正常显示*/
+      path: 'homeView',
+      name: 'Home',
+      component: () => import("@/views/HomeView"),  /*，该处是导入页面的另外一种方式，将HomeView界面进行二次路由导入*/
+    }
     ]
   },
   {
